ffb22cd7150aef9620e182da40bdf2924ddfb881
kenatman <76776206+kenatman@users.noreply.github.com>
2021-03-02 22:17:28 +0900

    middleware pt2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import "core-js";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
-const PORT = 3000;
+
+const PORT = 1000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
@@ -9,6 +15,12 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Hello from Home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
