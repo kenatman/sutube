3d016a7a7f6e700a3f7bf45a4f3a8d640b06fbf3
Cho yunsu <kenatman0723@gmail.com>
2021-03-05 22:51:14 +0900

    template variable / search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,16 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("video Detail");
-export const editVideo = (req, res) => res.render("edit Video");
-export const deleteVideo = (req, res) => res.render("delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("video Detail", { pageTitle: "Video detail" });
+export const editVideo = (req, res) =>
+  res.render("edit Video", { pageTitle: "Edit video" });
+export const deleteVideo = (req, res) =>
+  res.render("delete Video", { pageTitle: "Delete video" });
