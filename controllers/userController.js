fba430e55fc1409b58b9992eb788f30d9d948a0e
Cho yunsu <kenatman0723@gmail.com>
2021-03-07 15:00:39 +0900

    Connecting to MongoDB

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,6 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
--- db.js
+++ db.js
@@ -1,54 +1,15 @@
-export const videos = [
-  {
-    id: 11,
-    title: "Luffy",
-    description: "Onepiece",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 111,
-      name: "su",
-      email: "su@su.com",
-    },
-  },
-  {
-    id: 22,
-    title: "Zoro",
-    description: "Onepiece",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 111,
-      name: "su",
-      email: "su@su.com",
-    },
-  },
-  {
-    id: 33,
-    title: "Sanji",
-    description: "Onepiece",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 22,
-      name: "ssu",
-      email: "ssu@su.com",
-    },
-  },
-  {
-    id: 44,
-    title: "Franky",
-    description: "Onepiece",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 22,
-      name: "ssu",
-      email: "ssu@su.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/sutube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
