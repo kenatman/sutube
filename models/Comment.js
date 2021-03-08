25191522ac5765fa287fed67c85ed1d12662e209
Cho yunsu <kenatman0723@gmail.com>
2021-03-08 16:48:15 +0900

    comment model / home controller finished

---
+++ models/Comment.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+  text: { type: String, required: "Text is required" },
+  creatAt: { type: Date, default: Date.now },
+});
+
+const model = mongoose.model("Comment", CommentSchema);
+
+export default model;
