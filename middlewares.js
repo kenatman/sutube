ba1fb559331df49f67de2992702b8a7f29c7a7f3
Cho yunsu <kenatman0723@gmail.com>
2021-03-17 12:20:05 +0900

    front-end footer

--- middlewares.js
+++ middlewares.js
@@ -4,7 +4,7 @@ import multer from "multer";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Sutube";
   res.locals.routes = routes;
-  res.locals.user = { isAuthenticated: true, id: 123 };
+  res.locals.user = { isAuthenticated: false, id: 123 };
   next();
 };
 
