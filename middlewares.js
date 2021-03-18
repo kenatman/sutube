12ff4543c335a0b1d97c724230c41322ef5899d4
Cho yunsu <kenatman0723@gmail.com>
2021-03-18 17:29:06 +0900

    sessions with express

--- middlewares.js
+++ middlewares.js
@@ -4,7 +4,7 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Sutube";
   res.locals.routes = routes;
-  res.locals.user = req.user || {};
+  res.locals.user = req.user || null;
   next();
 };
 
