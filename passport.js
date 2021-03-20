2c2e35d732ad4fee557c2d115e15eb825480b848
Cho yunsu <kenatman0723@gmail.com>
2021-03-20 20:27:13 +0900

    github login

--- passport.js
+++ passport.js
@@ -1,7 +1,22 @@
 import passport from "passport";
+import { githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
+import routes from "./routes";
 
 passport.use(User.createStrategy());
+
+var GitHubStrategy = require("passport-github").Strategy;
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: `http://localhost:2500${routes.githubCallback}`,
+    },
+    githubLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,17 @@
 import express from "express";
+import passport from "passport";
 import {
   getJoin,
   postJoin,
   getLogin,
   postLogin,
   logout,
+  githubLoginCallback,
+  githubLogin,
+  postGithubLogIn,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
-import { onlyPublic } from "../middlewares";
+import { onlyPrivate, onlyPublic } from "../middlewares";
 import routes from "../routes";
 
 const globalRouter = express.Router();
@@ -19,7 +23,16 @@ globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogIn
+);
+
 globalRouter.get(routes.search, search);
 
 export default globalRouter;
