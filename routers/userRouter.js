3aeb1b0505eff25fd4dd489aa5f9f295be0842cd
Cho yunsu <kenatman0723@gmail.com>
2021-03-22 14:15:31 +0900

    profile / edit profile problem
    profile
    user.name problem
    edit profile input value problem
    upload profile photo problem

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
 import {
   changePassword,
-  editProfile,
+  getEditProfile,
+  postEditProfile,
   userDetail,
 } from "../controllers/userController";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
